69c8bf70a7ae52e26a4ef29bca27152bb7afd211
wardamukhtarkhan <102410054+wardamukhtarkhan@users.noreply.github.com>
2022-07-21 20:56:53 +0500

    Add files via upload
    ThingSpeak send data to computer using protocol http .  ThingSpeak is a client and NodeMCU work as serve

---
+++ using_protocol_http_to_send_data.ino
@@ -0,0 +1,48 @@
+#include <WiFiClient.h>
+#include <ESP8266WiFi.h>
+#include <DHT.h>
+#include <ESP8266HTTPClient.h>
+
+DHT dht(D5, DHT11);
+
+String host = "api.thingspeak.com";
+int httpPort = 80;  
+String url1 = "/update?api_key=E1UMR0GI90DLUWZM&field1=";
+String url2 = "/update?api_key=E1UMR0GI90DLUWZM&field2=";
+
+HTTPClient http; 
+WiFiClient client;
+void setup() 
+{
+  Serial.begin(115200);
+  WiFi.begin("Mooazam", "mooazam123");
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    delay(200);
+    Serial.print("..");
+  }
+  Serial.println();
+  Serial.println("NodeMCU is connected!");
+  Serial.println(WiFi.localIP());
+  dht.begin();
+}
+
+void loop() 
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  Serial.println("Temperature: " + String(t));
+  Serial.println("Humidity: " + String(h));
+
+  url1 = url1 + String(t);
+  http.begin(client , host,httpPort,url1); 
+  int httpCode = http.GET();
+  Serial.println(httpCode);
+  delay(2000);
+
+  url2 = url2 + String(h);
+  http.begin(client , host,httpPort,url2); 
+  httpCode = http.GET();
+  Serial.println(httpCode);
+  delay(2000);
+}
